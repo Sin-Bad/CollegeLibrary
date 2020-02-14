dc68ec14a1d238f2e2a519999b9ebde32517a209
Aditya Menon <adityamenon0@gmail.com>
2020-02-14 13:32:30 +0530

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.create(book_params)
+
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @book = Book.find(params[:id])
+
+    @book.destroy
+    redirect_to :action => :index
+  end
+  private
+
+  def book_params
+    params.require(:book).permit(:bookname, :authorname, :description)
+  end
+end
